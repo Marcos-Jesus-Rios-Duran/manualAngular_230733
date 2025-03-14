560333c9b2cb067759a055ddbb2024084013e738
Marcos <230733@utxicotepec.edu.mx>
2025-03-14 07:53:50 -0600

    Mejoras

--- manualAngular-230733/src/app/pages/ejercicio-11/ejercicio-11.component.ts
+++ manualAngular-230733/src/app/pages/ejercicio-11/ejercicio-11.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NgOptimizedImage } from '@angular/common';
+
 @Component({
   selector: 'app-ejercicio-11',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, CommonModule],
   templateUrl: './ejercicio-11.component.html',
-  styleUrl: './ejercicio-11.component.css'
+  styleUrls: ['./ejercicio-11.component.css']
 })
 export class Ejercicio11Component {
   logoUrl = './image.png';
