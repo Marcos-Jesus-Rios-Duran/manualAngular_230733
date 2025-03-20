f2cc45a48f13befa28d03c8826c35db12f097e48
Marcos <230733@utxicotepec.edu.mx>
2025-03-20 10:52:27 -0600

    Tablas API

---
+++ manualAngular-230733/src/app/tables/sessions-table/sessions-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sessions-table',
+  imports: [],
+  templateUrl: './sessions-table.component.html',
+  styleUrl: './sessions-table.component.css'
+})
+export class SessionsTableComponent {
+
+}
